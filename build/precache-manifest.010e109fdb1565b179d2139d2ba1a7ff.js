c4aed76f0b39eeedf8b1ae824ddd9ddae84bf533
unknown <mohaiminislam94@gmail.com>
2019-06-26 00:43:42 +0600

    build frontend and add Procfile for heroku

---
+++ build/precache-manifest.010e109fdb1565b179d2139d2ba1a7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b197ad9994afc0f90c3d713d442e0938",
+    "url": "/static/media/data-table.b197ad99.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "355099b64391e17881e5",
+    "url": "/static/js/main.e4006cee.chunk.js"
+  },
+  {
+    "revision": "af5bb0f98510fbb6664d",
+    "url": "/static/js/2.3436c280.chunk.js"
+  },
+  {
+    "revision": "355099b64391e17881e5",
+    "url": "/static/css/main.40ab2335.chunk.css"
+  },
+  {
+    "revision": "0821d2fbb6d76112c51da12ae4c50689",
+    "url": "/index.html"
+  }
+];
